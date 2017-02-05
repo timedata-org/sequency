6ab83f4c21c4033ae28d8ec0af4aa69c25dfcce5
Tom Ritchford <tom@swirly.com>
2017-02-05 17:10:48 +0100

    Add a test for the clock.

---
+++ test/clock_test.py
@@ -0,0 +1,43 @@
+import unittest
+from sequency.time.clock import Clock
+
+
+class MockTime(object):
+    def __init__(self, time):
+        self.time = time
+
+    def __call__(self):
+        return self.time
+
+
+class ClockTest(unittest.TestCase):
+    def test_simple(self):
+        system_time = MockTime(0)
+        clock = Clock(system_time=system_time)
+        self.assertEqual(clock.bpm, 120)
+        self.assertEqual(clock.to_beats_bars(61), (2.0, 30))
+        self.assertEqual(clock.time, 0)
+
+        system_time.time += 2
+        self.assertEqual(clock.time, 0)  # Not running.
+
+        clock.running = True
+        self.assertEqual(clock.time, 0)  # Running, but time hasn't advanced
+
+        system_time.time += 1
+        self.assertEqual(clock.time, 1)  # Running.
+
+        system_time.time += 3
+        self.assertEqual(clock.time, 4)  # Running.
+
+        clock.running = False
+        self.assertEqual(clock.time, 4)  # Not running.
+
+        system_time.time += 23
+        self.assertEqual(clock.time, 4)  # Not running.
+
+        clock.running = True
+        self.assertEqual(clock.time, 4)  # Not running.
+
+        system_time.time += 5
+        self.assertEqual(clock.time, 9)  # Not running.
